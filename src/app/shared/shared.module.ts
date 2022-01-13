85f6f65f9beed98ad654f316659e0c4cb44e109f
Kasun Abaywardana <kasun.a@tailwindsw.com>
2022-01-14 00:02:50 +0530

    Auth Guard service added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
+import { AuthGuardService } from './auth-guard.service';
 
 
 
@@ -9,6 +11,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  providers: [
+    AuthenticationService,
+    AuthGuardService
   ]
 })
 export class SharedModule { }
