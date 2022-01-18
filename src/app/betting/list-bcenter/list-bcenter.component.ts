0100d7805c6b6cbb423463e86f3352e3a5398210
Kasun Abaywardana <kasun.a@tailwindsw.com>
2022-01-18 23:51:39 +0530

    Table expand part

--- src/app/betting/list-bcenter/list-bcenter.component.ts
+++ src/app/betting/list-bcenter/list-bcenter.component.ts
@@ -14,7 +14,7 @@ export class ListBcenterComponent implements OnInit {
   public rowData = [
     [{ text: 'Bet 1 new' }, { text: '267 Galle Rd, Colombo 00300' },
     { text: 'Kasun Abaywardana' }, { text: '0 789 564 591' },
-    { text: 'fa fa-trash', isIcon: true }],
+    { text: 'fa fa-trash', isIcon: true },],
     [{ text: 'Bet 1 new' }, { text: '267 Galle Rd, Colombo 00300' },
     { text: 'Kasun Abaywardana' }, { text: '0 789 564 591' },
     { text: 'fa fa-trash', isIcon: true }]
