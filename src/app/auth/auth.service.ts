2ac39977591b9942db15b3d8b14faa3e72108def
Kasun Abaywardana <abaywardanakasun@gmail.com>
2022-02-06 14:21:05 +0530

    api data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_END_POINTS } from '../app.constant';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthService {
   constructor(public http: HttpClient) { }
 
   login(loginReq){
-    return this.http.post('url', loginReq);
+    return this.http.post(API_END_POINTS.LOGIN, loginReq);
   }
 
   testGetApi() {
