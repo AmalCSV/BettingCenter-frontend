6731f261e3585d95ca6d3c8d649a4dcf6c70b72b
Kasun Abaywardana <abaywardanakasun@gmail.com>
2022-02-06 15:35:46 +0530

    API changes

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,5 +1,5 @@
 import { environment} from "../environments/environment";
 
 export class API_END_POINTS {
-     public static LOGIN = environment.baseURL + 'users/login.php';
+     public static LOGIN = environment.baseURL + 'users/login';
 }
