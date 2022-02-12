4ecb5e8d1ea972937fedc78a574108c7be6b8fff
Kasun Abaywardana <abaywardanakasun@gmail.com>
2022-02-12 07:32:10 +0530

    User search function

--- src/app/user/create-user/create-user.component.ts
+++ src/app/user/create-user/create-user.component.ts
@@ -10,6 +10,7 @@ import { UserService } from '../user.service';
 })
 export class CreateUserComponent implements OnInit {
   public userList: Array<User>;
+  public displayUserList: Array<User>;
 
   public userForm = new FormGroup({
     firstName: new FormControl('', [Validators.required, Validators.minLength(4)]),
@@ -18,14 +19,12 @@ export class CreateUserComponent implements OnInit {
     password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
 
+  public searchForm = new FormGroup({
+    searchText: new FormControl('')
+  });
+
   public columnList = [
     "First Name", "Last Name", "User Name", "Action"
-  ];
-
-  public rowData = [
-    [{ text: 'Kasun' }, { text: 'Abaywardana' },
-    { text: 'kasun12' }, { text: 'fa fa-trash', isIcon: true }],
-    
   ];
 
   constructor(private userService: UserService) { }
@@ -51,6 +50,7 @@ export class CreateUserComponent implements OnInit {
     this.userService.getUserList().subscribe((res: any) => {
       if (res.success) {
         this.userList = User.list(res.data);
+        this.displayUserList = this.userList;
         console.log(this.userList);
       } else {
         console.log("Get user list error", JSON.stringify(res.data));
@@ -63,7 +63,7 @@ export class CreateUserComponent implements OnInit {
   createUser() {
     const userData = this.userForm.value;
     this.userService.createUser(new User(userData)).subscribe((res: any) => {
-      if(res.success) {
+      if (res.success) {
         this.getUserList();
       } else {
         console.log("Create user error", JSON.stringify(res.data));
@@ -71,6 +71,48 @@ export class CreateUserComponent implements OnInit {
     }, err => {
       console.log("Create user error", JSON.stringify(err));
     });
+  }
+
+  search() {
+    const search = this.searchForm.value.searchText.toLocaleLowerCase();
+
+    const firstName = this.userList.filter(f => {
+      const term = f.firstName ? f.firstName.toLocaleLowerCase(): '';
+      if (term.includes(search)) {
+        return f;
+      }
+    });
+
+    const lastName = this.userList.filter(f => {
+      const term = f.lastName ? f.lastName.toLocaleLowerCase(): '';
+      if (term.includes(search)) {
+        return f;
+      }
+    });
+
+    const userName = this.userList.filter(f => {
+      const term = f.userName ? f.userName.toLocaleLowerCase(): '';
+      if (term.includes(search)) {
+        return f;
+      }
+    });
+
+    if (search === '') {
+      this.displayUserList = this.userList;
+    } else if (userName.length>0) {
+      this.displayUserList = firstName;
+    } else if (firstName.length > 0) {
+      this.displayUserList = lastName;
+    } else {
+      this.displayUserList = userName;
+    }
+  }
+
+  reFill() {
+    const search = this.searchForm.value.searchText.toLocaleLowerCase();
+    if (search === '') {
+     this.displayUserList = this.userList;
+    }
   }
 
   get firstName() { return this.userForm.get('firstName'); }
