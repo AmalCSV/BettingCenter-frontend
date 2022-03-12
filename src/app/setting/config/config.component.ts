9c968cb617f0ee591eee146968cf2a3b53cb17b5
Kasun Abaywardana <abaywardanakasun@gmail.com>
2022-03-12 22:23:42 +0530

    Setting API form

--- src/app/setting/config/config.component.ts
+++ src/app/setting/config/config.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigComponent implements OnInit {
 
+  public settingForm = new FormGroup({
+    companyName: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    tax: new FormControl('', [Validators.required]),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  resetForm() {
+
+  }
+
+  updateForm(attribute) {
+
+  }
+
+  get companyName() { return this.settingForm.get('companyName'); }
+  get address() { return this.settingForm.get('address'); }
+  get tax() { return this.settingForm.get('tax'); }
 
 }
